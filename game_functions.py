7cabdc6fd356b3dfc3cedd0ad30c789d57d52a99
t517k <tk517@exeter.ac.uk>
2026-01-16 16:31:29 +0000

    adjustments

--- game_functions.py
+++ game_functions.py
@@ -22,4 +22,5 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    if letter in word:
+        
