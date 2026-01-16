bebe87d91b726c58d74d705689e578a273a94b1f
t517k <tk517@exeter.ac.uk>
2026-01-16 12:22:44 +0000

    Slight adjustment

--- game_functions.py
+++ game_functions.py
@@ -7,11 +7,11 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    if user_input == "h":
+    if user_input == 'h'
         if next_val > current_val:
             return True
         else: return False
-    elif user_input == "l":
+    elif user_input == 'l':
         if next_val < current_val:
             return True
         else:
