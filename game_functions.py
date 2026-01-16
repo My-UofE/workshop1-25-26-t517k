7c032503d0f6c601957902a5530437ff94137037
t517k <tk517@exeter.ac.uk>
2026-01-16 12:21:02 +0000

    Added functionality to game_2.py

--- game_functions.py
+++ game_functions.py
@@ -7,7 +7,15 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    pass
+    if user_input == "h":
+        if next_val > current_val:
+            return True
+        else: return False
+    elif user_input == "l":
+        if next_val < current_val:
+            return True
+        else:
+            return False
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
